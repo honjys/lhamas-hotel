154c00c5f42f27ead1909956c4ef855d6dd44c85
jhonWater <gustavo@gustavo>
2019-03-31 09:35:03 -0300

    Novo

--- app/database/TABELAS.sql
+++ app/database/TABELAS.sql
@@ -6,6 +6,10 @@
 	senha VARCHAR(51) NOT NULL,
 	PRIMARY KEY (cpf));
 
+insert into funcionario(cpf,nome,email,telefone,senha) values ('1111','janailson','janailsonsoares@','989182983','wheyprotein');
+insert into funcionario(cpf,nome,email, telefone,senha) values ('7788', 'micheal jackson','reidopop@','666999666','biliejean');
+insert into funcionario(cpf, nome, email, telefone, senha) values ('123','salami','salada@','28022017','123');
+
 CREATE TABLE cliente(
 	cpf VARCHAR(15) NOT NULL,
 	nome VARCHAR(250) NOT NULL,
@@ -13,12 +17,20 @@ CREATE TABLE cliente(
 	telefone VARCHAR(20) NOT NULL,
 	PRIMARY KEY (cpf));
 
-CREATe Table admin(cpf varchar(15) not null,
+
+insert into cliente(cpf, nome, email, telefone) values ('12332','irineu','vocenasabe@','40028922');
+insert into cliente(cpf, nome, email, telefone) values ('987789', 'bon scott', 'shotgirls@', '689843');
+
+
+CREATe Table admin(
+	cpf varchar(15) not null,
 	nome varchar(67) not null,
-	endereço varchar(200) not null,
 	email varchar(200) not null,
 	telefone varchar(20) not null,
+	senha varchar(80) not null,
 	primary key (cpf));
+insert into admin(cpf, nome, email, telefone, senha) values ('1','João Victor','tearsofthedragon@','87688768','123');
+insert into admin(cpf, nome, email, telefone, senha) values ('2','Isaque', 'Lovehurts@','22333446','1234');
 
 create table quartos(
 	numero varchar(3) not null,
@@ -38,10 +50,12 @@ insert into quartos(numero,metros2,aluguel_dia, status) values ('7','120','400',
 
 
 create table alugados(
-	cpf_funcio VARCHAR(15) NOT NULL,
+	cpf_cliente VARCHAR(15),
 	numero_quarto varchar(6) not null,
 	alugado_em date not null,
 	alugado_ate date not null,
 	primary key (numero_quarto),
-	foreign key (cpf_funcio) references funcionario(cpf),
+	foreign key (cpf_cliente) references cliente(cpf),
 	foreign key (numero_quarto) references quartos(numero));
+insert into alugados(cpf_cliente,numero_quarto,alugado_em,alugado_ate) values ('12332','2','2019-04-01','2019-04-02');
+insert into alugados(cpf_cliente,numero_quarto,alugado_em,alugado_ate) values ('987789', '7','2019-04-01','2019-04-04'); 
